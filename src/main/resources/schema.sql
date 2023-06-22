74a4b6add5ccfbfe992e57bafdacf24daf989ae6
id <id@AQ-0067>
2023-06-22 16:08:47 +0900

    データベースschema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,49 +1,53 @@
 -- テーブル削除
-DROP TABLE IF EXISTS order_details;
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS categories;
-DROP TABLE IF EXISTS customers;
-
--- カテゴリーテーブル
-CREATE TABLE categories
+--DROP TABLE IF EXISTS order_details;
+--DROP TABLE IF EXISTS orders;
+--DROP TABLE IF EXISTS items;
+--DROP TABLE IF EXISTS categories;
+--DROP TABLE IF EXISTS customers;
+-- アカウントテーブル
+CREATE TABLE account
 (
    id SERIAL PRIMARY KEY,
-   name TEXT
-);
--- 商品テーブル
-CREATE TABLE items
-(
-   id SERIAL PRIMARY KEY,
-   category_id INTEGER,
    name TEXT,
-   price INTEGER
+   email TEXT;
+   password TEXT,
+   FOREIGN KEY (authorise_id) REFERENCES authorise_name (authorise_id)
 );
--- 顧客テーブル
-CREATE TABLE customers
+-- 勤怠参照テーブル
+CREATE TABLE attendance
 (
-  id SERIAL PRIMARY KEY,
-  name TEXT,
-  address TEXT,
-  tel TEXT,
-  email TEXT,
-  password TEXT /*パスワード追加*/
+   FOREIGN KEY (id) REFERENCES account (id),
+   submit_date date,
+   FOREIGN KEY (attendance_id) REFERENCES attendance_type (attendance_id)
 );
-
--- 注文テーブル
-CREATE TABLE orders
+-- 休暇テーブル
+CREATE TABLE leave
 (
-  id SERIAL PRIMARY KEY,
-  customer_id INTEGER,
-  ordered_on DATE,
-  total_price INTEGER
+   FOREIGN KEY (id) REFERENCES account (id),
+   FOREIGN KEY (leave_id) REFERENCES leave_type (leave_id),
+   FOREIGN KEY (approval_id) REFERENCES approval_status (approval_id),
 );
-
--- 注文詳細テーブル
-CREATE TABLE order_details
+-- 勤怠状況テーブル（主キー）
+CREATE TABLE attendance_type
 (
-  id SERIAL PRIMARY KEY,
-  order_id INTEGER,
-  item_id INTEGER,
-  quantity INTEGER
-); 
+   attendance_id Integer PRIMARY KEY,
+   attendance_type text
+);
+-- 権限テーブル（主キー）
+CREATE TABLE authorise_name
+(
+   authorise_id Integer PRIMARY KEY,
+   authorise_name text
+);
+-- 休暇種類テーブル（主キー）
+CREATE TABLE leave_type
+(
+   leave_id Integer PRIMARY KEY,
+   leave_type text
+);
+-- 承認状態テーブル（主キー）
+CREATE TABLE approval_status
+(
+   approval_id Integer PRIMARY KEY,
+   approval_status text
+);
