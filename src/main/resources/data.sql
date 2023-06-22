68f67f6d344c54f256212e75de2ca43f9120e761
id <id@AQ-0038>
2023-06-22 16:16:27 +0900

    データ変更第１弾

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,11 @@
--- カテゴリーテーブルデータ
-INSERT INTO categories(name) VALUES('本');
-INSERT INTO categories(name) VALUES('DVD');
-INSERT INTO categories(name) VALUES('ゲーム');
--- 商品テーブルデータ
-INSERT INTO items(category_id, name, price) VALUES(1, 'Javaの基本', 2500);
-INSERT INTO items(category_id, name, price) VALUES(1, 'MLB Fun', 980);
-INSERT INTO items(category_id, name, price) VALUES(1, '料理BOOK!', 1200);
-INSERT INTO items(category_id, name, price) VALUES(2, 'なつかしのアニメシリーズ', 2000);
-INSERT INTO items(category_id, name, price) VALUES(2, 'The Racer', 1000);
-INSERT INTO items(category_id, name, price) VALUES(2, 'Space Wars 3', 1800);
-INSERT INTO items(category_id, name, price) VALUES(3, 'パズルゲーム', 780);
-INSERT INTO items(category_id, name, price) VALUES(3, 'Invader Fighter', 3400);
-INSERT INTO items(category_id, name, price) VALUES(3, 'Play the BascketBall', 2200);
+-- アカウントテーブルデータ
+INSERT INTO account(name) VALUES('鈴木一郎');
+INSERT INTO account(name) VALUES('鈴木二郎');
+INSERT INTO account(name) VALUES('鈴木三郎');
+INSERT INTO account(name) VALUES('佐藤二郎');
+
+-- アテンダンステーブルデータ
+--INSERT INTO attendance(submit_date, attendance_id) VALUES(, date, );
+--INSERT INTO items(category_id, name, price) VALUES(1, 'MLB Fun', 980); 
+
+--アテンダンスタイプテーブル
