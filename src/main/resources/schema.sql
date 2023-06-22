e484d5d2b14b864d26e82c0eecebb330ef8efacc
id <id@AQ-0067>
2023-06-22 16:44:35 +0900

    sql変更

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,32 +1,3 @@
--- テーブル削除
---DROP TABLE IF EXISTS order_details;
---DROP TABLE IF EXISTS orders;
---DROP TABLE IF EXISTS items;
---DROP TABLE IF EXISTS categories;
---DROP TABLE IF EXISTS customers;
--- アカウントテーブル
-CREATE TABLE account
-(
-   id SERIAL PRIMARY KEY,
-   name TEXT,
-   email TEXT;
-   password TEXT,
-   FOREIGN KEY (authorise_id) REFERENCES authorise_name (authorise_id)
-);
--- 勤怠参照テーブル
-CREATE TABLE attendance
-(
-   FOREIGN KEY (id) REFERENCES account (id),
-   submit_date date,
-   FOREIGN KEY (attendance_id) REFERENCES attendance_type (attendance_id)
-);
--- 休暇テーブル
-CREATE TABLE leave
-(
-   FOREIGN KEY (id) REFERENCES account (id),
-   FOREIGN KEY (leave_id) REFERENCES leave_type (leave_id),
-   FOREIGN KEY (approval_id) REFERENCES approval_status (approval_id),
-);
 -- 勤怠状況テーブル（主キー）
 CREATE TABLE attendance_type
 (
@@ -50,4 +21,27 @@ CREATE TABLE approval_status
 (
    approval_id Integer PRIMARY KEY,
    approval_status text
+);
+-- アカウントテーブル
+CREATE TABLE account
+(
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   email TEXT,
+   password TEXT,
+   authorise_id INTEGER REFERENCES authorise_name (authorise_id)
+);
+-- 勤怠参照テーブル
+CREATE TABLE attendance
+(
+   id SERIAL REFERENCES account (id),
+   submit_date date,
+   attendance_id INTEGER REFERENCES attendance_type (attendance_id)
+);
+-- 休暇テーブル
+CREATE TABLE leave
+(
+   id SERIAL REFERENCES account (id),
+   leave_id Integer REFERENCES leave_type (leave_id),
+   approval_id Integer REFERENCES approval_status (approval_id),
 );
