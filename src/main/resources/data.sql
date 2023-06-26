7348f7fc1d83e3adf0fbbfd14d87f6e69230a1b2
id <id@AQ-0038>
2023-06-26 09:32:39 +0900

    sql_3弾

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,8 @@ INSERT INTO account(name, email, password, authorise_id) VALUES('鈴木一郎', 
 INSERT INTO account(name, email, password, authorise_id) VALUES('鈴木一郎', 'sato2@abc.com','sato2020', 2);
 
 -- アテンダンステーブルデータ
---INSERT INTO attendance(submit_date, attendance_id) VALUES(, date, );
---INSERT INTO items(category_id, name, price) VALUES(1, 'MLB Fun', 980); 
+INSERT INTO attendance(submit_date, attendance_id) VALUES(date, 1);
+INSERT INTO attendance(submit_date, attendance_id) VALUES(date, 2); 
 
 --アテンダンスタイプテーブル
 INSERT INTO attendance_type(attendance_id, attendance_type) VALUES(1,'出勤');
@@ -20,21 +20,21 @@ INSERT INTO authorise_name(authorise_id,authorise_name) VALUES(1,'上位');
 INSERT INTO authorise_name(authorise_id,authorise_name) VALUES(2, '下位');
 
 --leave_typeテーブル
-INSERT INTO leave_type(leave_type) VALUES('全日有給休暇');
-INSERT INTO leave_type(leave_type) VALUES('午前有給休暇');
-INSERT INTO leave_type(leave_type) VALUES('午後有給休暇');
-INSERT INTO leave_type(leave_type) VALUES('全日傷病休暇');
-INSERT INTO leave_type(leave_type) VALUES('午前傷病休暇');
-INSERT INTO leave_type(leave_type) VALUES('午後傷病休暇');
-INSERT INTO leave_type(leave_type) VALUES('休職');
-INSERT INTO leave_type(leave_type) VALUES('産休');
-INSERT INTO leave_type(leave_type) VALUES('育児休暇');
-INSERT INTO leave_type(leave_type) VALUES('介護休暇');
-INSERT INTO leave_type(leave_type) VALUES('連続特別休暇');
-INSERT INTO leave_type(leave_type) VALUES('その他休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(1, '全日有給休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(2, '午前有給休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(3, '午後有給休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(4, '全日傷病休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(5, '午前傷病休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(6, '午後傷病休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(7, '休職');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(8, '産休');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(9, '育児休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(10, '介護休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(11, '連続特別休暇');
+INSERT INTO leave_type(leave_id, leave_type) VALUES(12, 'その他休暇');
 
 --approval_statusテーブル
-INSERT INTO approval_status(approval_status) VALUES('未申請')
-INSERT INTO approval_status(approval_status) VALUES('承認待ち')
-INSERT INTO approval_status(approval_status) VALUES('差し戻し')
-INSERT INTO approval_status(approval_status) VALUES('承認済み')
+INSERT INTO approval_status(approval_id, approval_status) VALUES(1, '未申請')
+INSERT INTO approval_status(approval_id, approval_status) VALUES(2, '承認待ち')
+INSERT INTO approval_status(approval_id, approval_status) VALUES(3, '差し戻し')
+INSERT INTO approval_status(approval_id, approval_status) VALUES(4, '承認済み')
