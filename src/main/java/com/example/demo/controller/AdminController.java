c802b7826adb97d1d48cf78662bdb125190b81d0
id <id@AQ-0038>
2023-06-26 16:31:35 +0900

    adminController　途中

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -58,6 +59,7 @@ public class AdminController {
 	//アカウント編集
 	@GetMapping("/admin/{id}/edit")
 	public String editAccount(
+			@PathVariable(name="id") Integer id,
 			@RequestParam(name= "name") String name,
 			@RequestParam(name= "email", defaultValue="") String email,
 			@RequestParam(name= "password", defaultValue= "") String password,
