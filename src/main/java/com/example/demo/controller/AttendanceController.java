38f7255d6b72a44380bf897118f14900983bd42c
id <id@AQ-0067>
2023-06-26 16:27:22 +0900

    Merge branch 'master' of https://github.com/itsme-yuzuki/pairProgramming.git

--- src/main/java/com/example/demo/controller/AttendanceController.java
+++ src/main/java/com/example/demo/controller/AttendanceController.java
@@ -1,41 +1,32 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.demo.model.Account;
-import com.example.demo.repository.CustomerRepository;
-
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class AttendanceController {
 
-	@Autowired
-	HttpSession session;
-
-	@Autowired
-	Account account;
-
-	@Autowired
-	CustomerRepository customerRepository;
-
-	// ログイン画面を表示
-	@GetMapping("index")
-	public String index(
-			@RequestParam(name = "page", defaultValue = "") String page,
-			Model model) {
-
-		// エラーパラメータのチェック
-		if (page.equals("勤怠管理")) {
-			return "kintai";
-		} else if (page.equals("休暇管理")) {
-			return "kyuuka_kannri";
-		}
-		return "attendaceMenu";
-	}
+//	@Autowired
+//	HttpSession session;
+//
+//	@Autowired
+//	Account account;
+//
+//	@Autowired
+//	CustomerRepository customerRepository;
+//
+//	// ログイン画面を表示
+//	@GetMapping("index")
+//	public String index(
+//			@RequestParam(name = "page", defaultValue = "") String page,
+//			Model model) {
+//
+//		// エラーパラメータのチェック
+//		if (page.equals("勤怠管理")) {
+//			return "kintai";
+//		} else if (page.equals("休暇管理")) {
+//			return "kyuuka_kannri";
+//		}
+//		return "attendaceMenu";
+//	}
 
 }
--- src/main/java/com/example/demo/entity/Account.java
+++ src/main/java/com/example/demo/entity/Account.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import org.springframework.stereotype.Component;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+@Component
 @Data
 @Entity
 @Table(name = "account")
@@ -19,31 +22,36 @@ public class Account {
 	private String name; // 名前
 
 	private String email; // メールアドレス
-	
+
 	private String password; //　パスワード
-	
+
 	@Column(name = "authorise_id")
 	public Integer authoriseId; // 権限
+
+	//コンストラクタ	
+	public Account() {
+
+	}
 
 	public Account(Integer id, String password) {
 		this.id = id;
 		this.password = password;
 	}
-	
-	//コンストラクタ
-	public Account() {
-		
-	}
-	
+
 	public Account(String name, String email,
-			String password, Integer authoriseId){
-		this.name= name;
-		this.email= email;
-		this.password= password;
-		this.authoriseId= authoriseId;
-		
-		
+			String password, Integer authoriseId) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.authoriseId = authoriseId;
 	}
-	
-	
+
+	public Account(Integer id, String name, String email, String password, Integer authoriseId) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.authoriseId = authoriseId;
+	}
+
 }
