3f805c396ed146c5261d857be810dc0ad265db1e
id <id@AQ-0038>
2023-06-27 13:13:23 +0900

    pwdResetGen 途中

---
+++ src/main/java/com/example/demo/controller/PasswordResetController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Account;
+import com.example.demo.model.PasswordResetGenerator;
+import com.example.demo.model.User;
+import com.example.demo.repository.AccountRepository;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class PasswordResetController {
+
+	@Autowired
+	HttpSession session;
+	
+	@Autowired
+	User user;
+	
+	@Autowired
+	PasswordResetGenerator passwordResetGenerator;
+
+	@Autowired
+	AccountRepository accountRepository;
+
+	@Autowired
+	Account account;
+
+	//仮パスワードを生成
+	@PostMapping("/reset")
+	public void reset() {
+		PasswordResetGenerator pwdGen = new PasswordResetGenerator();
+	
+	//データベースに仮パスワードを登録
+		Account account= new Account(id, password);
+		
+		accountRepository.save(account);
+	
+	//登録した仮パスワードをメールアドレスに送信
+	
+	}
+}
---
+++ src/main/java/com/example/demo/model/PasswordResetGenerator.java
@@ -0,0 +1,36 @@
+package com.example.demo.model;
+
+public class PasswordResetGenerator {
+	static String getRandomString(int i) {
+		String theAlphaNumericS;
+		StringBuilder builder;
+
+		theAlphaNumericS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+				+ "0123456789";
+
+		//create the StringBuffer
+		builder = new StringBuilder(i);
+
+		for (int m = 0; m < i; m++) {
+
+			// generate numeric
+			int myindex = (int) (theAlphaNumericS.length()
+					* Math.random());
+			//add the characters
+			builder.append(theAlphaNumericS
+					.charAt(myindex));
+		}
+
+		return builder.toString();
+	}
+
+	public static String main(String[] args) {
+		// the random string length
+		int i = 12;
+
+		// output 
+		return (getRandomString(i));
+		
+//		System.out.println("A random string: " + getRandomString(i));
+	}
+}
