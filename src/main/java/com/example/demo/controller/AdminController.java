132668f88ec98682014f1fca007c40e6b1a9238f
id <id@AQ-0038>
2023-06-27 11:25:28 +0900

    adminController 完成

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -30,7 +30,7 @@ public class AdminController {
 	@GetMapping("/admin")
 	public String index(
 		Model model) {
-		List<Account> accounts = accountRepository.findAll();
+		List<Account> accounts = accountRepository.findAllByOrderById();
 		model.addAttribute("accounts", accounts);
 			return "admin";
 		}
@@ -53,23 +53,46 @@ public class AdminController {
 		Account account= new Account(name, email, password, authoriseId);
 		accountRepository.save(account);
 		
-		return "redirect:/account";
+		return "redirect:/admin";
 	}
 	
 	//アカウント編集
 	@GetMapping("/admin/{id}/edit")
 	public String editAccount(
-			@PathVariable(name="id") Integer id,
-			@RequestParam(name= "name") String name,
-			@RequestParam(name= "email", defaultValue="") String email,
-			@RequestParam(name= "password", defaultValue= "") String password,
-			@RequestParam(name= "authorise_id", defaultValue="") Integer authoriseId,
+			@PathVariable("id") Integer id,
 			Model model
 			) {
-		Account account= new Account(name, email, password, authoriseId);
+		account = accountRepository.findById(id).get();
 		model.addAttribute("account", account);
 			
 		return "adminEdit";
+	}
+	
+	//アカウント編集登録
+	@PostMapping("/admin/edit")
+	public String editAccount(
+			@RequestParam("id") Integer id,
+			@RequestParam("name") String name,
+			@RequestParam("email") String email,
+			@RequestParam("password") String password,
+			@RequestParam("authoriseId") Integer authoriseId,
+			Model model
+			) {
+		Account account= new Account(id, name, email, password, authoriseId);
+		accountRepository.save(account);
+		
+		return "redirect:/admin";
+	}
+	
+	//アカウント削除
+	@PostMapping("/admin/{id}/delete")
+	public String deleteAccount(
+			@PathVariable("id")Integer id,
+			Model model
+			) {
+		accountRepository.deleteById(id);
+		
+		return "redirect:/account";
 	}
 	
 }
--- src/main/java/com/example/demo/repository/AccountRepository.java
+++ src/main/java/com/example/demo/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,6 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
 	
 	Optional<Account> findByIdAndEmail(Integer id, String email);
 	
+	List<Account>findAllByOrderById();
+
 }
