1b552862ace7e739648bb2c72fcfbaf3a921c2e1
id <id@AQ-0067>
2023-06-28 16:04:19 +0900

    メール最終確認

--- src/main/java/com/example/demo/controller/MailController.java
+++ src/main/java/com/example/demo/controller/MailController.java
@@ -3,9 +3,9 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
-@RestController
+@Component
 public class MailController {
 	
 	@Autowired
