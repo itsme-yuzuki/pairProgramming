cb96b42ee33a9871e627beb8a65064389668b279
id <id@AQ-0038>
2023-06-30 10:44:15 +0900

    attendance repository変更

--- src/main/java/com/example/demo/repository/AttendanceRepository.java
+++ src/main/java/com/example/demo/repository/AttendanceRepository.java
@@ -1,6 +1,5 @@
 package com.example.demo.repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Attendance;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
-	List<Attendance> findByAccountIdOrderByDate(Integer accountId);
+	List<Attendance> findByDateAndAccountId(Integer accountId);
 	
-	Optional<Attendance> findByDate(LocalDate date);
+	Optional<Attendance> findByDateAndAccountId(String date, Integer accountId);
 }
