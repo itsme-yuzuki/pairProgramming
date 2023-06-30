260f8d30e03c2fbaf532271a2ce955d5e314fecf
id <id@AQ-0038>
2023-06-30 10:20:40 +0900

    attendanceRepository 変更

--- src/main/java/com/example/demo/repository/AttendanceRepository.java
+++ src/main/java/com/example/demo/repository/AttendanceRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,8 +11,5 @@ import com.example.demo.entity.Attendance;
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
 	List<Attendance> findByAccountIdOrderByDate(Integer accountId);
 	
-	Optional<Attendance> findByDate(LocalDate date);
-	
-	//SELECT submit_date, DATE_PART('dow', submit_date) AS dayofWeek FROM attendance
-	@Query("select submit_date, date_part('dow', submit_date) as dayofWeek from attendance = ?1")
+	Optional<Attendance> findByDate(LocalDate date);	
 }
